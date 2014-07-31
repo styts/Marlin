7a3a88e152de7db49b46143fa631f4085d93241b
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2014-08-01 01:17:31 +0200

    Narrower XYZ field

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -363,11 +363,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 205
+#define X_MAX_POS 150
 #define X_MIN_POS 0
-#define Y_MAX_POS 205
+#define Y_MAX_POS 140
 #define Y_MIN_POS 0
-#define Z_MAX_POS 200
+#define Z_MAX_POS 150
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
