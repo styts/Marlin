b80ae7e189cdd0af8c402979c8e974b331e26689
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2014-08-03 15:55:25 +0200

    More limits adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -365,9 +365,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // Travel limits after homing
 #define X_MAX_POS 150 // tested
 #define X_MIN_POS 0
-#define Y_MAX_POS 180 // tested
+#define Y_MAX_POS 170 // tested (with hotbed)
 #define Y_MIN_POS 0
-#define Z_MAX_POS 100 // untested 
+#define Z_MAX_POS 114 // untested 
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
