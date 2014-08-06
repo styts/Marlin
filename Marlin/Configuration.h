739624506ed96ddbb0651f48fabe1a2212f14ba0
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2014-08-07 00:18:01 +0200

    Shorten X due to new belt tensioner

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -363,11 +363,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 150 // tested
+#define X_MAX_POS 132 // tested
 #define X_MIN_POS 0
-#define Y_MAX_POS 170 // tested (with hotbed)
+#define Y_MAX_POS 170 // tested
 #define Y_MIN_POS 0
-#define Z_MAX_POS 114 // untested 
+#define Z_MAX_POS 114 // tested 
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
