bec5e2b41568682e567549dc1ef70e38c4b39ab6
Kirill Stytsenko <kirill@pro.local>
2014-11-09 14:04:17 +0100

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -365,7 +365,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // Travel limits after homing
 #define X_MAX_POS 124 // tested
 #define X_MIN_POS 0
-#define Y_MAX_POS 170 // tested
+#define Y_MAX_POS 160 // tested
 #define Y_MIN_POS 0
 #define Z_MAX_POS 114 // tested 
 #define Z_MIN_POS 0
@@ -481,12 +481,12 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,4000,760*1.1}  // default steps per unit for Ultimaker
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,4000,760*1.1}  //tested. default steps per unit for Ultimaker
+#define DEFAULT_MAX_FEEDRATE          {900, 900, 2, 20}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {1500, 1500, 100, 10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
+#define DEFAULT_ACCELERATION          2000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  2000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
 
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
 // The offset has to be X=0, Y=0 for the extruder 0 hotend (default extruder).
@@ -496,8 +496,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
 #define DEFAULT_XYJERK                20.0    // (mm/sec)
-#define DEFAULT_ZJERK                 0.4     // (mm/sec)
-#define DEFAULT_EJERK                 5.0    // (mm/sec)
+#define DEFAULT_ZJERK                 999// 0.4     // (mm/sec)
+#define DEFAULT_EJERK                 999//5.0    // (mm/sec)
 
 //===========================================================================
 //=============================Additional Features===========================
